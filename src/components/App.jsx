cd630b3d436cf4b0513e7daffa62f1f5d749222d
Maria-Tkh <mcweaver117@gmail.com>
2021-08-31 10:18:21 +0300

    hw-01

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import { Profile } from 'components/Profile/Profile';
+import user from '../user.json';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import statisticalData from '../statistical-data.json';
+
+export const AppProfile = () => {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+export const AppStatistics = () => {
+  return (
+    <>
+      <Statistics title="Upload stats" />
+      <Statistics stats={statisticalData} />
+    </>
+  );
+};
+
+// //////////////////////////////////
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import { StatItem } from 'components/Statistics/StatItem';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h1 className={css.title}>{title}</h1>
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <StatItem key={id} label={label} percentage={percentage} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
