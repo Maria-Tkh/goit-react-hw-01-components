048268a69719e021e9fac54cac9f91495796b36a
Maria-Tkh <mcweaver117@gmail.com>
2021-09-03 20:14:00 +0300

    proptypes corr

--- src/components/Friends/FriendList/FriendList.jsx
+++ src/components/Friends/FriendList/FriendList.jsx
@@ -20,7 +20,7 @@ export const FriendList = ({ friends }) => {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
     }),
   ),
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { StatItem } from 'components/Statistics/StatItem';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section class={css.statistics}>
+    <section className={css.statistics}>
       {title && <h1 className={css.title}>{title}</h1>}
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
